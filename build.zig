7cde0eb91f2658541d9dc21ff7229c82c45e5732
Filipe Azevedo <filipe@azevedo.io>
2024-02-18 17:29:46 +0000

    maybe?

--- build.zig
+++ build.zig
@@ -1,9 +1,14 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build) void {
+pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
-
     const optimize = b.standardOptimizeOption(.{});
+
+    const module = b.addModule("known-folders", .{
+        .source_file = .{ .path = "known-folders.zig" },
+    });
+
+    try b.modules.put(b.dupe("known-folders"), module);
 
     const lib = b.addStaticLibrary(.{
         .name = "known-folders",
