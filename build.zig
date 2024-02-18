d6ba6a9b99009b1cf9ee6323519dd26de808b46a
Filipe Azevedo <filipe@azevedo.io>
2024-02-18 17:17:06 +0000

    fix build

--- build.zig
+++ build.zig
@@ -2,6 +2,6 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     _ = b.addModule("known-folders", .{
-        .root_source_file = .{ .path = "known-folders.zig" },
+        .source_file = .{ .path = "known-folders.zig" },
     });
 }
