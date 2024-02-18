6f64e6fe95f4348b203b2ea6ce2b002bbbcdba47
Filipe Azevedo <filipe@azevedo.io>
2024-02-18 17:23:43 +0000

    maybe?

--- build.zig
+++ build.zig
@@ -1,7 +1,18 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    _ = b.addModule("known-folders", .{
-        .source_file = .{ .path = "known-folders.zig" },
+    const target = b.standardTargetOptions(.{});
+
+    const optimize = b.standardOptimizeOption(.{});
+
+    const lib = b.addStaticLibrary(.{
+        .name = "known-folders",
+        // In this case the main source file is merely a path, however, in more
+        // complicated build scripts, this could be a generated file.
+        .root_source_file = .{ .path = "known-folders.zig" },
+        .target = target,
+        .optimize = optimize,
     });
+
+    b.installArtifact(lib);
 }
